63b132f2d9482a53f6734ba809e423bdf13b8e41
logan-anderson <landerson2@upei.ca>
2021-01-11 17:00:26 -0400

    updated img

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,10 @@
 import markdownStyles from './markdown-styles.module.css'
 import BlockContent from '@sanity/block-content-to-react'
-
+import { options } from '../lib/sanity'
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <BlockContent blocks={content} className={markdownStyles.markdown} />
+      <BlockContent projectId={options.projectId} dataset={options.dataset} blocks={content} className={markdownStyles.markdown} />
     </div>
   )
 }
